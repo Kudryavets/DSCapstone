1a667ba7eff7e85e4d8f7efb1cd76bbb8816f51a
Kudryavets <akudryavets@gmail.com>
2016-05-30 18:08:00 +0300

    global.R for help function for server.R created

---
+++ global.R
@@ -0,0 +1,22 @@
+source("main/meta.model.R")
+source("main/text.processing.R")
+
+get.profanity.vec <- function() readRDS("main/sources/profanity.rds"))
+
+get.models <- function() meta.model.load("main/sources/")
+
+get.status <- function() c("text.processing status: OK", "Ngrams.building status: OK", "Ngrams.count status: OK", 
+                           "model.build status: OK", "model.get.row status: OK", "meta.model.build status: OK", 
+                           "meta.model.load status: OK", "meta.model.predict status: OK", "meta.model.evaluate status: OK",
+                           "all systems status: OK")
+
+get.summary <- function() c("model.size: 10 Mb", "model.speed: 1000 queries for 10 s", "accuracy by precise matching: 3",
+                            "accuracy by probability rstimation: 0.4", "accuracy by order: 5", "perplexity per word: 1.37")
+
+get.instructions <- function() "This is TSafer, interactive app for the next word predictng. It uses interpolated Kneser-Ney smoothing for 4,3,2,1 grams and back-off model for unseen words.
+Please, try this app, enter your phrase into the predicting area and check the unswer.
+At the bottom you can see the word cloud, which seems to be the best illustration for the predicted words probabilities.
+For some digits about the predictions click Probability Table.
+For some digits about the model click Model Summary.
+You can see model logs to the right. 
+Have fun!"
